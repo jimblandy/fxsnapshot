f0813db2c9212d2951859abed8aa9cedb314fed0
Jim Blandy <jimb@red-bean.com>
2018-09-13 12:34:20 -0700

    Put pb_rs-generated protobuf reader in $OUT_DIR, not source dir.

--- build.rs
+++ build.rs
@@ -1,21 +1,34 @@
 extern crate lalrpop;
 extern crate pb_rs;
 
-use std::path::PathBuf;
+use std::env;
+use std::fs::File;
+use std::io::Write;
+use std::path::{Path, PathBuf};
 use pb_rs::types::{Config, FileDescriptor};
 
 fn main() {
+    let out_dir = env::var("OUT_DIR").unwrap();
+
     let config = Config {
         in_file: PathBuf::from("src/dump/CoreDump.proto"),
-        out_file: PathBuf::from("src/dump/CoreDump.proto.rs"),
-        single_module: false,
         import_search_path: vec![PathBuf::from("src/dump")],
+        out_file: Path::new(&out_dir).join("out"),
+        single_module: false,
         no_output: false,
         error_cycle: true,
     };
     FileDescriptor::write_proto(&config)
         .expect("failed to generate CoreDump.rs from CoreDump.proto");
     println!("cargo:rerun-if-changed=src/dump/CoreDump.proto");
+
+    // Since it's impossible to `include!` a file that starts with `//!`
+    // comments, as all `pb_rs`-generated modules do, we need to generate our
+    // own module file that we can include.
+    File::create(Path::new(&out_dir).join("generated.rs"))
+        .expect("error creating $OUT_DIR/generated.rs in build.rs")
+        .write_all(b"pub mod mozilla;")
+        .expect("error writing to generated.rs from build.rs");
 
     lalrpop::Configuration::new()
         .process_file("src/query/grammar.lalrpop")
