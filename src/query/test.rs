ba33e721c5d21307883b560ad5621d685891a953
Jim Blandy <jimb@red-bean.com>
2018-10-13 19:13:17 -0700

    Test output from parser.

--- src/query/test.rs
+++ src/query/test.rs
@@ -1,11 +1,46 @@
 #![cfg(test)]
 
 use super::QueryParser;
+use super::ast::{Expr, Predicate, PredicateOp, Var};
+
+// Quick functions for building expressions.
+fn root() -> Box<Expr> {
+    Box::new(Expr::Var(Var::Root))
+}
+
+fn nodes() -> Box<Expr> {
+    Box::new(Expr::Var(Var::Nodes))
+}
+
+fn pred_op(stream: Box<Expr>, op: PredicateOp, pred: Box<Predicate>) -> Box<Expr> {
+    Box::new(Expr::Predicate(stream, op, *pred))
+}
+
+fn filter(stream: Box<Expr>, pred: Box<Predicate>) -> Box<Expr> {
+    pred_op(stream, PredicateOp::Filter, pred)
+}
+
+fn field(name: &str, pred: Box<Predicate>) -> Box<Predicate> {
+    Box::new(Predicate::Field(name.to_owned(), pred))
+}
+
+fn number(n: u64) -> Box<Expr> {
+    Box::new(Expr::Number(n))
+}
+
+fn expr_pred(expr: Box<Expr>) -> Box<Predicate> {
+    Box::new(Predicate::Expr(expr))
+}
+
+fn and1(pred: Box<Predicate>) -> Box<Predicate> {
+    Box::new(Predicate::And(vec![*pred]))
+}
 
 #[test]
 fn parse_query() {
-    QueryParser::new().parse("root")
-        .expect("parse failed");
-    QueryParser::new().parse("nodes { id: 0x0123456789abcdef }")
-        .expect("parse failed");
+    assert_eq!(QueryParser::new().parse("root").expect("parse failed"),
+               root());
+    assert_eq!(QueryParser::new().parse("nodes { id: 0x0123456789abcdef }")
+               .expect("parse failed"),
+               filter(nodes(), and1(field("id", expr_pred(number(0x0123456789abcdef))))));
 }
