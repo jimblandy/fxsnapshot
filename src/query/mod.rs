73f97f6f9ab749e7d9755b219038731488d08a49
Jim Blandy <jimb@red-bean.com>
2018-10-15 11:28:09 -0700

    query: Remove references to type-checking from module docs.

--- src/query/mod.rs
+++ src/query/mod.rs
@@ -1,4 +1,4 @@
-//! Parsing, type-checking, planning, and executing queries.
+//! Parsing, planning, and executing queries.
 //!
 //! We use a few central types and traits for running queries:
 //!
@@ -7,7 +7,8 @@
 //!
 //! - The [`Plan`][plan] trait is for types that represent a specific plan of
 //!   execution. This has a `run` method which actually returns the value
-//!   resulting from a particular evaluation.
+//!   resulting from a particular evaluation. Queries are compiled to a
+//!   `Box<Plan>`.
 //!
 //! - The [`DynEnv`][dye] struct represents the dynamic environment in which
 //!   evaluation takes place. At the moment, this just carries the `CoreDump`
@@ -17,30 +18,24 @@
 //! - The [`Value`][value] type represents a run-time value: a number, string, node,
 //!   edge, or stream thereof.
 //!
-//! A `Expr` represents an expression that is syntactically well-formed,
-//! but may not be well-typed, and may not correspond to the evaluation strategy
-//! we want to use. For example:
-//!
-//!     10 { id: 20 }
-//!
-//! is syntactically well-formed, but integers don't have fields, and are not
-//! streams to be filtered, so this expression is meaningless. Or consider:
+//! An `Expr` represents an expression that is well-formed syntactically, but
+//! may not correspond to the evaluation strategy we want to use. For example:
 //!
 //!     nodes { id: 0x12345 }
 //!
-//! This expression is well-typed, but producing a stream of all the nodes just
-//! to find the one with the given id is extremely inefficient. This should
-//! simply check the `CoreDump` for a node with the given id, and produce a
-//! stream of zero or one nodes.
+//! This expression produces a stream of all the nodes in the `CoreDump` just to
+//! find the one with the given id. This is extremely inefficient: it should
+//! simply try to look up the node with the given id, and produce a stream of
+//! zero or one nodes.
 //!
 //! Evaluating by simply walking the `Expr` directly is straightforward, but it
-//! intertwines the code for checking types and performing optimizations with
-//! the code for actually carrying out the computation, so everything gets a
-//! little more difficult to work with.
+//! intertwines the code for performing optimizations with the code for actually
+//! carrying out the computation, so everything gets a little more difficult to
+//! work with.
 //!
-//! Instead, we perform type checking and optimization ('planning') up front,
-//! and produce a tree of values that implements the `Plan` trait, which can be
-//! run to yield a `Value` or an `Error`.
+//! Instead, we perform optimization ('planning') up front, and produce a tree
+//! of values that implements the `Plan` trait, which can be run to yield a
+//! `Value` or an `Error`.
 //!
 //! [expr](ast/enum.Expr.html)
 //! [plan](run/trait.Plan.html)
