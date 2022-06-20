a956191d2c8690b0f1cefeb844bda50fb81ac349
Jim Blandy <jimb@red-bean.com>
2022-06-20 14:55:56 -0700

    Update to quick-protobuf 0.8.0 and pb-rs 0.9.1.

--- build.rs
+++ build.rs
@@ -1,38 +1,40 @@
-extern crate lalrpop;
-extern crate pb_rs;
-
-use pb_rs::types::{Config, FileDescriptor};
+use pb_rs::{ConfigBuilder, types::FileDescriptor};
 use std::env;
-use std::fs::File;
-use std::io::Write;
-use std::path::{Path, PathBuf};
+use std::path::Path;
 
-fn main() {
-    let out_dir = env::var("OUT_DIR").unwrap();
+fn generate_coredump_parser(manifest_dir: &str, out_dir: &str) {
+    let out_dir = Path::new(out_dir).join("dump");
+    if !out_dir.is_dir() {
+        std::fs::DirBuilder::new().create(&out_dir).unwrap();
+    }
 
-    let config = Config {
-        in_file: PathBuf::from("src/dump/CoreDump.proto"),
-        import_search_path: vec![PathBuf::from("src/dump")],
-        out_file: Path::new(&out_dir).join("out"),
-        single_module: false,
-        no_output: false,
-        error_cycle: true,
-    };
-    FileDescriptor::write_proto(&config)
-        .expect("failed to generate CoreDump.rs from CoreDump.proto");
-    println!("cargo:rerun-if-changed=src/dump/CoreDump.proto");
+    let proto_file = "src/dump/CoreDump.proto";
+    println!("cargo:rerun-if-changed={}", proto_file);
+    let config_builder = ConfigBuilder::new(
+        &[Path::new(manifest_dir).join(proto_file)],
+        None,
+        Some(&out_dir),
+        &[],
+    ).unwrap();
 
-    // Since it's impossible to `include!` a file that starts with `//!`
-    // comments, as all `pb_rs`-generated modules do, we need to generate our
-    // own module file that we can include.
-    File::create(Path::new(&out_dir).join("generated.rs"))
-        .expect("error creating $OUT_DIR/generated.rs in build.rs")
-        .write_all(b"pub mod mozilla;")
-        .expect("error writing to generated.rs from build.rs");
+    FileDescriptor::run(&config_builder.build()).unwrap();
+}
 
+fn generate_query_parser() {
     lalrpop::Configuration::new()
         .use_cargo_dir_conventions()
         .process_file("src/query/query.lalrpop")
         .expect("failed to generate parser");
     println!("cargo:rerun-if-changed=src/query/query.lalrpop");
+}
+
+fn main() {
+    let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let out_dir = env::var("OUT_DIR").unwrap();
+
+    dbg!(&manifest_dir);
+    dbg!(&out_dir);
+
+    generate_coredump_parser(&manifest_dir, &out_dir);
+    generate_query_parser();
 }
